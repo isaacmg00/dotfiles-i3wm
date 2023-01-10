c207d93d45a492329151e208cbb52b82b91e917a
Isaac <43230315+isaacmg00@users.noreply.github.com>
2023-01-09 23:51:39 -0800

    Delete polybar directory

--- polybar/tinybar.sh
+++
@@ -1,9 +0,0 @@
-# script to Tiny-launch polybar
-
-#!/usr/bin/env bash
-
-echo "---" | tee -a /tmp/polybar2.log
-polybar volume (name of your bar here instead of tray) >> /tmp/polybar2.log 2>&1
-
-~                                                                                                    
-~   
